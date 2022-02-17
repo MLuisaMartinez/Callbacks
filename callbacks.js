21d32bf404cdee9d1a27e910cf96cd7a55c8ed74
MLuisa Martinez <95011604+MLuisaMartinez@users.noreply.github.com>
2022-02-17 10:23:24 -0600

    Add files via upload

---
+++ callbacks.js
@@ -0,0 +1,18 @@
+///1///
+<script>
+functionName ReverseString(str) {
+    return str.split('').reverse().join('');
+   
+ // Function call 
+ document.write(ReverseString("Rise and Shine"))
+ ///2////
+ const str = 'Hello World!';
+console.log(str.toUpperCase());  // expected output: HELLO WORLD!
+ </script>
+///3////
+
+functionName repMaster(x,y){
+    console.log(y); 
+    console.log(y); 
+   }
+   repMaster(“Almost the weekend”,”I finished this practice”);
